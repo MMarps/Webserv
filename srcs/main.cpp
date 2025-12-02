be7f3070381338128993cc765b603440cbb1356b
Jonatan Le Douaron <jle-doua@f2r10s15.clusters.42paris.fr>
2025-12-02 17:32:47 +0100

    SERVEUR SOCKET OK

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,17 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmarpaul <mmarpaul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jle-doua <jle-doua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 17:47:25 by mmarpaul          #+#    #+#             */
-/*   Updated: 2025/11/28 18:43:52 by mmarpaul         ###   ########.fr       */
+/*   Updated: 2025/12/02 17:17:51 by jle-doua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Webserv.hpp"
 
-int	main(int ac, char **av) {
+int	main(int ac, char **av)
+{
+	int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+	int clientFd;
+	socklen_t addrSize;
+    struct sockaddr_in myAddr, otherAddr;
+	int opt = 1;
 
+	if (serverFd == -1)
+	{
+		std::cout << "pb serverfd" << std::endl;
+		return (1);
+	}
+		
+	if(setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+	{
+		std::cout << "pb setsock" << std::endl;
+		return (1);
+	}	
 
+	memset(&myAddr, 0, sizeof(myAddr));
+    myAddr.sin_family = AF_INET;
+    myAddr.sin_addr.s_addr = htonl(INADDR_ANY);   // écoute toutes les interfaces
+    myAddr.sin_port = htons(8080);   
+
+	if (bind(serverFd, (struct sockaddr *) &myAddr,sizeof(myAddr)) == -1)
+	{
+		std::cout << "pb bind : " << errno << std::endl;
+		return (1);
+	}	
+	if (listen(serverFd, SOMAXCONN) == -1)
+	{
+		
+		std::cout << "pb listen" << std::endl;
+		return (1);
+	}	
+	while (1)
+	{
+		addrSize = sizeof(otherAddr);
+		clientFd = accept(serverFd, (struct sockaddr *)&otherAddr, &addrSize);
+		if (clientFd == -1)
+			return (1);
+		std::cout << "nice" << std::endl;
+	}
+	
+	
+	
 	return (0);
 }
